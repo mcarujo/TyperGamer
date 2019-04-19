5788917a9027e9bb07176616c3cb9b76b10fdf65
Marco Carujo <mamcarujo@gmail.com>
2019-04-19 14:40:11 -0300

    verify

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
-var tempoinicial = "10";
+var tempoinicial = "2";
 
-$(document).ready(function () {
+$(document).ready(function() {
   resetGame();
   controller(tempoinicial);
   wordsCounterTextArea();
   setWordsNumber();
+  verify();
 });
-
 
 function wordsCounter() {
   var text = $(".frase").text();
@@ -21,7 +21,7 @@ function setWordsNumber() {
 
 function wordsCounterTextArea() {
   var text = $("#textarea");
-  text.on("input", function () {
+  text.on("input", function() {
     var value = text.val();
     var chars = chars == " " ? 0 : value.length;
     var words = value.split(/[\S]+/).length;
@@ -33,9 +33,9 @@ function wordsCounterTextArea() {
 function controller(timeStart) {
   $("#seconds").text(timeStart);
   var field = $("#textarea");
-  field.one("focus", function () {
+  field.one("focus", function() {
     var time = $("#seconds").text();
-    var pid = setInterval(function () {
+    var pid = setInterval(function() {
       time--;
       $("#seconds").text(time);
       if (time < 1) {
@@ -46,17 +46,33 @@ function controller(timeStart) {
     }, 1000);
   });
 }
-function verify(){
-var frase = $(".frase").text();
+function verify() {
+  var phrase = $(".frase");
+  var text = $("#textarea");
+  text.on("input", function() {
+    var typed = text.val();
+    var reference = phrase.text().substring(0, typed.length);
+    if (typed == reference) {
+      text.removeClass("wrong");
+      text.addClass("right");
+    } else {
+      text.removeClass("right");
+      text.addClass("wrong");
+    }
+  });
 }
+
 function resetGame() {
-  $("#reset").click(function () {
-    $("#textarea").attr("disabled", false);
-    $("#textarea").toggleClass("off");
-    $("#textarea").val("");
+  $("#reset").click(function() {
+    var text = $("#textarea");
+    text.attr("disabled", false);
+    text.removeClass("wrong");
+    text.removeClass("right");
+    text.removeClass("off");
+    text.val("");
     $("#userChars").text("0");
     $("#userWords").text("0");
-    $("#seconds").text("10");
+    $("#seconds").text(tempoinicial);
     controller();
   });
 }
